277ca28ccd282f29844459a4d8c2258e073ffdfa
Roman Stetsiuk <stetcukroman@gmail.com>
2017-03-23 14:56:17 +0200

    task 1

--- 1.js
+++ 1.js
@@ -1,1 +1,16 @@
-test;
+/*
+Write a JavaScript function to check whether an `input` is an array or not. Go to the editor
+Test Data :
+console.log(is_array('w3resource')); //false
+console.log(is_array([1, 2, 4, 0])); //true
+*/
+  function isArr(input) {
+    return (toString.call(input) ==="[object Array]") ?
+        true : false;
+}
+
+  var arr = [,,,,];
+  alert(isArr([1,2,3])); //true
+  alert(isArr(1)); // false
+  alert(isArr(arr)); //true
+  alert(isArr("asdasd")); // false
