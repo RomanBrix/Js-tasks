244a6ea54576f3d70eb037734a0a9942ead694a7
Roman Stetsiuk <stetcukroman@gmail.com>
2017-03-24 10:12:00 +0200

    Added function with recursion

--- 3.js
+++ 3.js
@@ -4,17 +4,31 @@ in range (x, y). Go to the editor
 Example : range(2, 9)
 Expected Output : [3, 4, 5, 6, 7, 8]
 */
-function getInt(a, b) {
+
+//
+//Функция с использованием рекурсии
+//
+function getInt(start, end) {
+  if (end - start === 2) {
+    return [start + 1];
+  } else {
+      var answ = getInt(start, end - 1);
+      answ.push(end - 1);
+      return answ;
+    }
+}  
+
+alert(getInt(2,9));  //[3, 4, 5, 6, 7, 8]
+
+/*
+function getInt(start, end) {
   var answ = [];
   while(a < b){
     ++a;
     answ.push(a);
   }
   answ.pop();
-
   return answ;
-
-};
-
-
+ }
 alert(getInt(10,20));
+*/
