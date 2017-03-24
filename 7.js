8c1f9839c3c9bf9bea4761155bb6de89f01da460
Roman Stetsiuk <stetcukroman@gmail.com>
2017-03-24 10:25:18 +0200

    Add function with recursion

--- 7.js
+++ 7.js
@@ -1,6 +1,23 @@
 //Write a JavaScript program to check whether a number is even or not.
+
+
+/*
 function even(a) {
   return (Math.abs(a) % 2 == 1) ? false : true;
 }
 
 alert(even(423231325421387));//false
+*/
+
+
+function even(a)  {
+  if (a === 0) {
+    return true;
+  }
+  if (a === 1) {
+    return false;
+  } else {
+    return even(Math.abs(a-=2));
+  }  
+}
+alert(even(-63437)); //false
