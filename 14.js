1c407445605f30360d0774fe43aad560e57fd980
Roman Stetsiuk <stetcukroman@gmail.com>
2017-03-25 17:02:48 +0200

    remove duplicate items

--- 14.js
+++ 14.js
@@ -1,16 +1,16 @@
 //Write a JavaScript program to remove duplicate
 //items from an array (ignore case sensitivity).
 function removeDupl(arr) {
-  var out=[],
+  var answ=[],
       obj={};
 
   for (var i=0; i<arr.length; i++) {
     obj[arr[i]]=0;
   }
   for (i in obj) {
-    out.push(i);
+    answ.push(i);
   }
-  return out;
+  return answ;
 }
 var testArr = [1, 2, 2, 4,4,4,4,4,,5, 4, 7, 8, 7, 3, 6,,,];
-alert(removeDupl(testArr))
+alert(removeDupl(testArr));
