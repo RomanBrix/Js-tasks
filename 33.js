6b15069c2041da3d733b8dc13814035005a5fb69
Roman Stetsiuk <stetcukroman@gmail.com>
2017-03-26 15:07:25 +0300

    script to empty an array keeping the original

---
+++ 33.js
@@ -0,0 +1,5 @@
+//Write a JavaScript script to empty an array keeping the original.
+var testArr = [1,23,4,3,5,6,6,4,34,23,2];
+
+testArr.length = 0; // самый быстрый способ очистить массив
+console.log(testArr);
