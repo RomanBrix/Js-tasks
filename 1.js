53996589cce088cbfd022c2de23040e6ee348f28
Roman Stetsiuk <stetcukroman@gmail.com>
2017-03-27 10:20:19 +0300

    reverse function

--- 1.js
+++ 1.js
@@ -1,1 +1,16 @@
-//testasdasdasdasd
+/*
+Write a JavaScript function that reverse a number.
+Example x = 32243;
+Expected Output : 34223
+*/
+
+
+function revers(numbers) {
+  var arr = [],
+      answ;
+  arr = String(numbers).split('').reverse();
+  answ = Number(arr.join(''));
+  console.log(typeof answ);
+  return answ;
+}
+console.log(revers(32243)); //34223
