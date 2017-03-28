f1a0eced7efe22788203fb12303d99dd5824b7c2
Roman Stetsiuk <stetcukroman@gmail.com>
2017-03-28 17:49:56 +0300

    list the properties

--- 1.js
+++ 1.js
@@ -1,7 +1,31 @@
 /**
  * Created by RomanBrix on 3/28/17.
  */
-//test
-function fun() {
+/*
+ Write a JavaScript program to list the properties of a JavaScript object.
+ Sample object :
+ var student = {
+ name : "David Rayy",
+ sclass : "VI",
+ rollno : 12 };
+ Sample Output : name,sclass,rollno
+ */
+var student = {
+    name : "David Rayy",
+    sclass : "VI",
+    rollno : 12 };
+function lookProp(obj) {
+
+    var keys = [],
+        props = [];
+    for (var a in obj){
+        keys.push(a);
+        props.push(obj[a]);
+    }
+    console.log( keys );
+    console.log( props );
+
 
 }
+
+lookProp(student);
