1d1b4015e62ed0c3c997ae62b632338f4d23bb56
Roman Stetsiuk <stetcukroman@gmail.com>
2017-03-31 10:11:12 +0300

    find the longest (new)

--- 6.js
+++ 6.js
@@ -1,23 +1,34 @@
 /*
-Write a JavaScript function that accepts a string as a parameter and
-find the longest word within the string.
+ Write a JavaScript function that accepts a string as a parameter and
+ find the longest word within the string.
 
-Example string : 'Web Development Tutorial'
-Expected Output : 'Development'
-*/
+ Example string : 'Web Development Tutorial'
+ Expected Output : 'Development'
+ */
 
 function longestWord(string) {
-  var arr = string.split(" "),
-      longest = "";
+    var arr = string.split(" "),
+        longest = "";
 
-  for (var i = 0; i < arr.length; i++) {
-    for (var j = 0; j < arr.length; j++) {
-      if(arr[i].length > arr[j].length && arr[i].length > longest.length){
-        longest = arr[i];
-      }
+    // for (var i = 0; i < arr.length; i++) {
+    //   for (var j = 0; j < arr.length; j++) {
+    //     if(arr[i].length > arr[j].length && arr[i].length > longest.length){
+    //       longest = arr[i];
+    //     }
+    //   }
+    // }
+
+
+    function fn(a, b) {
+        return (a.length < b.length) ?  1 : -1;
     }
-  }
-  return longest;
+    arr.sort(fn);
+    return arr[0];
 }
 
-console.log(longestWord("Web Development Tutorial"));
+console.log(longestWord("Web Development " +
+                        "xxxxxxcxnvmafcnxmvadfxncmasfvnxcv " +
+                        "fskdbfkjsdhfjsdkfh " +
+                        "Tutorial" +
+                        " aasdkaskfmalkfnalskjd"));
+
