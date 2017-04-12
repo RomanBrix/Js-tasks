bf7c191f38a63bab05b330817572ba3080d30548
Roman Stetsiuk <stetcukroman@gmail.com>
2017-04-12 14:02:29 +0300

    Sorted array

---
+++ arrow_function_2.js
@@ -0,0 +1,58 @@
+/*
+
+ The code for this exercise implements a wrapper for working with sorted arrays.
+ Its constructor takes a comparison function that compares two elements and returns a number,
+ negative if the first is less than the second, zero when they are equal, and positive otherwise
+ (similar to what the sort method on arrays expects).
+
+
+ Rewrite the code to use an ES6 class. Then, rewrite the loop to use the ES6 array method findIndex,
+ which is like indexOf, but takes a function instead of an element as argument,
+ and returns the index of the first element for which that function returns true
+ (or returns -1 if no such element was found).
+ For example [1, 2, 3].findIndex(x => x > 1) yields 1. Use arrow functions for all function expressions.
+
+
+
+ function SortedArray(compare) {
+ this.compare = compare
+ this.content = []
+ }
+ SortedArray.prototype.findPos = function(elt) {
+ for (var i = 0; i < this.content.length; i++) {
+ if (this.compare(elt, this.content[i]) < 0) break
+ }
+ return i
+ }
+ SortedArray.prototype.insert = function(elt) {
+ this.content.splice(this.findPos(elt), 0, elt)
+ }
+
+ var sorted = new SortedArray(function(a, b) { return a - b })
+ sorted.insert(5)
+ sorted.insert(1)
+ sorted.insert(2)
+ sorted.insert(4)
+ sorted.insert(3)
+ console.log("array:", sorted.content)
+
+
+ */
+
+ class SortedArray {
+     constructor(how){
+         this.how = how;
+         this.content = [];
+         this.insert = (ins) =>{
+             this.content.push(ins);
+             return this.content.sort(how);
+         };
+     }
+ }
+let sorted = new SortedArray((a, b) => a - b);
+sorted.insert(5);
+sorted.insert(1);
+sorted.insert(2);
+sorted.insert(4);
+sorted.insert(3);
+console.log("array:", sorted.content);
